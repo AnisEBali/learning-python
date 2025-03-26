975af6c1705e8026ea7abfd501e57b8481f3fb02
AEBali <aniselbali1997@hotmail.com>
2025-03-26 20:48:35 +0100

    more dictionary exploration

--- Test variable types/dictionary.py
+++ Test variable types/dictionary.py
@@ -4,3 +4,18 @@ my_dictionary = {
 }
 
 print(my_dictionary['apple'])
+
+conflict_dictionary = {
+    'cat' : 'a goofy animal',
+    'dog' : 'a happy animal',
+    'cat' : 'a lazy animal'
+}
+
+print(conflict_dictionary['cat'])
+# Will only show the latest entry 'a lazy animal'
+
+two_entries = {
+    'monday' : 'a hateful day',
+    'friday' : 'the best day'
+}
+print(two_entries['friday']+ ", " + two_entries['monday'])
